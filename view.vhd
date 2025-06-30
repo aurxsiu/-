72c64e52703e5bb892189733678d92d1272240e3
aurxsiu <xiushou71@qq.com>
2025-06-30 21:34:17 +0800

    第一天实验结束,重写了逻辑框架,用来适配Verilog,VHDL太牛逼了,啥都能放一起,Verilog啥都不行. 去除了SySW,直接在第一次触发W1时赋值SWA/B/C,因为此时W1/2/3在赋值语句中为000,我也不知道为什么 将SSTO作为了普通reg变量,处理取指流程时在第一个W2产生时期赋1.然后在第二个W1中被STO侦测赋STO为1,同时通过STO赋指令变量来间接影响指令执行变量,由此达到和祖传代码一致的时序流程,即没有W3.无需修改指令执行变量的赋值语句

--- view.vhd
+++
@@ -1,9 +0,0 @@
-signal SySW: std_logic;
-begin
-    process(SySW)
-    begin
-        if (SySW'event and SySW = '1') then		
-			-- synchronizate SWC/B/A if SySW is at the rising edge
-				SWC <= RSWC; SWB <= RSWB; SWA <= RSWA;
-			end if;
-    end process
